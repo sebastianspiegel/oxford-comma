8edea74cf7d995395d07d5fda53af4c257268513
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2020-10-22 12:57:17 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        array.join
+    elsif array.size == 2
+        array.join (" and ")
+    else
+        new_string = array.join ",, "
+        new_string << ", and"
+        new_array = new_string.split(", ")
+        new_array[-1], new_array[-2] = new_array[-2], new_array[-1]
+        new_array.join " "
+    end
 end
